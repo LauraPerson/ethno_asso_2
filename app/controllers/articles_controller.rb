1ead7f7c948e79e83a1ffccb51b876d3da90325e
laura person <lauraperson29800@gmail.com>
2022-07-01 11:19:39 +0200

    controllers

---
+++ app/controllers/articles_controller.rb
@@ -0,0 +1,38 @@
+class ArticlesController < ApplicationController
+  skip_before_action :authenticate_user!, only: [:index, :show]
+
+
+  def index
+    @articles = Article.all
+  end
+
+  def show 
+    @article = Article.find(params[:id])
+    authorize @article
+  end
+
+
+
+  def new
+    @article = Article.new
+    authorize @article
+
+  end 
+
+  def create
+    @article = Article.new(article_params)
+    @article.user = current_user
+    authorize @article
+    @article.save 
+    redirect_to article_path(@article)
+
+  end
+
+  private 
+
+  def article_params 
+    params.require(:article).permit(:title, :content)
+  end 
+
+
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,27 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_06_30_155730) do
+ActiveRecord::Schema[7.0].define(version: 2022_06_30_160946) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "articles", force: :cascade do |t|
+    t.string "title"
+    t.text "content"
+    t.bigint "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_articles_on_user_id"
+  end
+
+  create_table "ressources", force: :cascade do |t|
+    t.string "title"
+    t.text "content"
+    t.bigint "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_ressources_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -22,8 +40,15 @@ ActiveRecord::Schema[7.0].define(version: 2022_06_30_155730) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "first_name"
+    t.string "last_name"
+    t.text "description"
+    t.boolean "super_admin", default: false
+    t.boolean "admin", default: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "articles", "users"
+  add_foreign_key "ressources", "users"
 end
