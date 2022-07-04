4bda860b21c551a74f36a71e18d742a8dcb65c12
laura person <lauraperson29800@gmail.com>
2022-07-04 15:58:07 +0200

    added typewriter

--- app/javascript/controllers/hello_controller.js
+++ app/javascript/controllers/hello_controller.js
@@ -1,7 +1,9 @@
 import { Controller } from "@hotwired/stimulus"
 
+// Connects to data-controller="navbar"
 export default class extends Controller {
   connect() {
-    this.element.textContent = "Hello World!"
+
+    console.log("Test Hello")
   }
 }
