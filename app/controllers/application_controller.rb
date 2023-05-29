ebfd966e42bbc3d716f121d3528b508d14c796a1
laura person <lauraperson29800@gmail.com>
2023-05-29 15:29:02 +0200

    added meta data

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,9 @@ class ApplicationController < ActionController::Base
   before_action :authenticate_user!
   include Pundit::Authorization
 
+  def default_url_options
+    { host: ENV["DOMAIN"] || "localhost:3000" }
+  end
   
 end
 
