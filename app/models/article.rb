245c85050c3b18222f00ed1f5d689969ce68bb93
laura person <lauraperson29800@gmail.com>
2023-05-29 17:03:47 +0200

    added validation in article model + added resources

--- app/models/article.rb
+++ app/models/article.rb
@@ -2,6 +2,8 @@ class Article < ApplicationRecord
   belongs_to :user
   has_many_attached :photos
   has_rich_text :content
+  validates :title, presence: true
+  validates :filter, presence: true
 
   FILTERS = ["Ethnographie urbaine", "Discriminations", "Écologie"]
 end
